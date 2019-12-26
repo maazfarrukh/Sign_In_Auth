aa888c9fb1e95eb87437c0b72b56c78f45af3f98
Moaz <maazfarrukh@gmail.com>
2019-12-26 15:23:42 +0500

    update sign_in.py

--- sign_in.py
+++ sign_in.py
@@ -7,6 +7,7 @@ class SignInAuthenticator:
         self.cur = self.db.cursor()
 
     def main(self):
+        self.choice('Do you want to sign up(Y/N): ')
         self.login()
 
     def choice(self, prompt):
@@ -24,24 +25,24 @@ class SignInAuthenticator:
         email = input('email: ')
         password = input('password: ')
 
-        self.cur.execute(f"INSERT INTO users(email,password) VALUES('{email}', '{password}')")
+        self.cur.execute(f"INSERT INTO users(email,password)  VALUES('{email}', '{password}') ")
         self.db.commit()
         print('now u can login')
         self.login()
 
     def login(self):
-        print('Write ur email and password')
-        email = input('email: ')
-        password = input('pass: ')
-        self.cur.execute(f"SELECT * FROM users WHERE email='{email}' AND password='{password}'")
-        if self.cur.fetchone() is not None:
-            print(f'WELCOME {email} ur now logged in!')
-            exit()
-        else:
-            print('Invalid username or password')
-            self.choice('Do you want to sign up(Y/N)')
-            self.login()
-            exit()
+        limit = 0
+        while limit < 5:
+            limit += 1
+            print('Write ur email and password')
+            email = input('email: ')
+            password = input('pass: ')
+            self.cur.execute(f"SELECT * FROM users WHERE email='{email}' AND password='{password}'")
+            if self.cur.fetchone() is not None:
+                print(f'WELCOME ur now logged in!')
+                exit()
+            else:
+                print('Invalid username or password')
 
 
 SignInAuthenticator().main()
